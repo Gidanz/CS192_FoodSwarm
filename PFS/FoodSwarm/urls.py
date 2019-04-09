044a219a6a1135f32bf30d26d7bdfd346352ee9a
Gelo Rosel <gelo.rosel@gmail.com>
2019-04-09 16:43:35 +0800

    updated font end with dynamic webpages

--- PFS/FoodSwarm/urls.py
+++ PFS/FoodSwarm/urls.py
@@ -4,4 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='FoodSwarm-Home'),
     path('about/', views.about, name='FoodSwarm-About'),
+    path('page/<int:pk>', views.PageDetailView.as_view(), name='FoodSwarm-Page'),
+    path('update/<int:pk>', views.UpdateDetailView.as_view(), name='FoodSwarm-Update'),
+    path('profile/', views.profile, name='FoodSwarm-Profile'),
+    path('favorites/', views.favorites, name='FoodSwarm-Favorites'),
+    path('nearme/', views.nearme, name='FoodSwarm-Nearme'),
+    path('report/', views.report, name='FoodSwarm-Report'),
+    path('request/', views.request, name='FoodSwarm-Request'),
 ]
--- PFS/FoodSwarm/views.py
+++ PFS/FoodSwarm/views.py
@@ -1,13 +1,27 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic.detail import DetailView
 from .models import FoodStall
+
+from FoodSwarm.models import FoodStall
 
 # Create your views here.
 
 def home(request):
+	title = "Home"
 	foodstalls = {
 		'foodstalls': FoodStall.objects.all()
 	}
 	return render(request, 'FoodSwarm/home.html', foodstalls)
+
+class PageDetailView(DetailView):
+	template_name = 'FoodSwarm/foodstall_detail.html'
+	model = FoodStall
+
+class UpdateDetailView(DetailView):
+	template_name = 'FoodSwarm/update_detail.html'
+	model = FoodStall
 
 def comments(request):
 	comments = {
@@ -15,6 +29,20 @@ def comments(request):
 	}
 	return render(request, 'FoodSwarm/home.html', comments)
 
-
 def about(request):
 	return render(request, 'FoodSwarm/about.html', {'title': 'About'})
+
+def profile(request):
+	return render(request, 'FoodSwarm/profile.html', {'title': 'Profile'})
+
+def favorites(request):
+	raise Http404('page does not exist')
+
+def nearme(request):
+    raise Http404('page does not exist')
+
+def report(request):
+	raise Http404('page does not exist')
+
+def request(request):
+	raise Http404('page does not exist')
